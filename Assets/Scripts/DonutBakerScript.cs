e1f7faca4cb3b06754f5e3de0d53819070282ffb
SkyWhy14 <artursgravitis312@gmail.com>
2026-02-03 09:12:59 +0200

    Colaider izmantošana

---
+++ Assets/Scripts/DonutBakerScript.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using UnityEngine;
+
+public class DonutBakerScript : MonoBehaviour
+{
+    public GameObject[] donutPrefabs; // Changed from GameObject donutPrefab to GameObject[] donutPrefabs
+    public float bakeInterval = 1.0f;
+    float minpoz, maxpoz;
+    Transform ovenTransform;
+
+    void Start()
+    {
+        ovenTransform = GetComponent<Transform>();
+    }
+
+    public void BakeDonut(bool state)
+    {
+        if (state)
+        {
+            StartCoroutine(SpawnDonut());
+        }
+        else
+        {
+            StopAllCoroutines();
+        }
+    }
+
+    IEnumerator SpawnDonut()
+    {
+        while (true)
+        {
+            minpoz = ovenTransform.position.x - 40.0f;
+            maxpoz = ovenTransform.position.x + 40.0f;
+            float randPoz = Random.Range(minpoz, maxpoz);
+            Vector2 spawnPoz = new Vector2(randPoz, ovenTransform.position.y);
+
+            int donutIndex = Random.Range(0, donutPrefabs.Length); // Use donutPrefabs.Length
+            Instantiate(donutPrefabs[donutIndex], spawnPoz, Quaternion.identity,ovenTransform);
+
+            yield return new WaitForSeconds(bakeInterval);
+        }
+    }
+}
---
+++ Assets/Scripts/Drag.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Drag : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IDragHandler, IEndDragHandler
+{
+    private RectTransform rectTransform;
+    private Canvas canvas;
+    SFX_Script script;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        script = FindFirstObjectByType<SFX_Script>();
+        rectTransform = GetComponent<RectTransform>();
+    }
+
+    // Implementing IPointerDownHandler
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        Debug.Log("Izdarīts klikšķis uz velkamā objekta");
+        script.PlaySFX(1);
+    }
+
+    // Implementing IBeginDragHandler
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        Debug.Log("Sākts vilkšanas process");
+    }
+
+    // Implementing IDragHandler
+    public void OnDrag(PointerEventData eventData)
+    {
+        Debug.Log("Objekts tiek vilkts");
+        Vector2 mousePosition = eventData.position;
+        mousePosition.x = Mathf.Clamp(mousePosition.x,
+            0 + rectTransform.rect.width / 2, Screen.width - rectTransform.rect.width / 2);
+
+        mousePosition.y = Mathf.Clamp(mousePosition.y,
+           0 + rectTransform.rect.height / 2, Screen.height - rectTransform.rect.height / 2);
+
+        rectTransform.position = mousePosition;
+    }
+
+    // Implementing IEndDragHandler
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        Debug.Log("Vilkšanas process pabeigts");
+    }
+}
